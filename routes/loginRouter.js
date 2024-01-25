f0b73be4381ba00e8f473cce6cee09b98d687c65
RalfiSlask <nilssonnarki@gmail.com>
2024-01-25 10:31:54 +0100

    feat: add crypto security for login

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -30,43 +30,46 @@ router.post("/register", (req, res) => {
         email: email,
         password: encryptedPassword,
       };
+      console.log(encryptedPassword);
       users.push(newUser);
       console.log(users, newUser);
-      fs.writeFile("./logins.json", JSON.stringify(users), (writeErr) => {
-        if (writeErr) {
-          res.status(500).json({ writeErr: "Cant read files" });
-          console.log("writeerror");
-        } else {
-          res.json({ read: "can read file" });
+      fs.writeFile(
+        "./logins.json",
+        JSON.stringify(users, null, 2),
+        (writeErr) => {
+          if (writeErr) {
+            res.status(500).json({ writeErr: "Cant read files" });
+            console.log("writeerror");
+          } else {
+            res.json({ read: "can read file" });
+          }
         }
-      });
+      );
     }
   });
 });
 
 router.post("", (req, res) => {
-  console.log(req.body);
   fs.readFile("./logins.json", (err, loginData) => {
     if (err) {
       res.status(500).json({ err: "could not find login data" });
     } else {
       const { email, password } = req.body;
       const key = "Salt Nyckel";
-      const decryptedPassword = CryptoJS.AES.decrypt(password, key).toString(
-        CryptoJS.enc.Utf8
-      );
-      console.log(decryptedPassword);
+      const encryptedPassword = getEncryptedPassword(password, key);
+
       const loginArray = JSON.parse(loginData);
-      const user = loginArray.find((user) => {
-        if (user.email === email) {
-          return true;
-        }
-        return false;
-      });
+      const user = loginArray.find((user) => user.email === email);
       if (user) {
-        res.send("hejsan");
-        console.log("hej");
+        const decryptedPassword = CryptoJS.AES.decrypt(
+          encryptedPassword,
+          key
+        ).toString(CryptoJS.enc.Utf8);
+        if (password === decryptedPassword) {
+          res.send("Login succesfull");
+        }
       } else {
+        console.log(encryptedPassword);
         res.json({ exist: "does not exist" });
         console.log("user not found on server/database");
       }
